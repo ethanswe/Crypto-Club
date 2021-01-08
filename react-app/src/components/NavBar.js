bf140b7e21f416626f418df79fe2af1c86d46b97
ethanswe <ethanaschnitzer@gmail.com>
2021-01-08 13:29:13 -0700

    initial homepage styling

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import styled from 'styled-components';
+import Logo from '../imgs/logo.jpg';
+
+const Nav = styled.nav`
+display: flex;
+justify-content: flex-end;
+/* align-items: center; */
+flex-direction: row;
+align-items: flex-end;
+text-decoration: none;
+/* margin: 0 auto; */
+max-height: 50px;
+background-color: black;
+`
+const Container = styled.div`
+background-color: black;
+`
+const LogoNav = styled.img.attrs({
+    src: `${Logo}`
+})`
+width: 80px;
+height: 80px;
+max-width: 80px;
+max-height: 80px;
+/* margin: 0 auto; */
+display: flex;
+`
 
 const NavBar = ({ authenticated, setAuthenticated }) => {
   let location = useLocation();
   const currentPage = location.pathname;
   // console.log(currentPage);
   return (
-    <nav>
+    <Container>
+      <NavLink to="/" exact={true} activeClassName="active">
+        <LogoNav />
+      </NavLink>
+    <Nav>
     {currentPage === "/" ? 
       <>
-          <NavLink to="/" exact={true} activeClassName="active">
-        Home
-          </NavLink>
           <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
@@ -28,8 +56,8 @@ const NavBar = ({ authenticated, setAuthenticated }) => {
         </>
         : ""}
   
-    </nav >
-
+    </Nav >
+    </Container>
   );
 }
 
